06e9b5ff89047bf91955d54283bfec19f8044399
will-rice-dev <will.rice.dev@gmail.com>
2024-03-28 14:40:49 -0400

    Gave up and added daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+import daisyui from 'daisyui'
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: [daisyui],
+  theme: {},
+  daisyui: {
+    themes: ["light", "dark", "retro"],
+  },
+};
