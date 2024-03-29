717b918a1530c7394289570c40e263061452a7e3
will-rice-dev <will.rice.dev@gmail.com>
2024-03-28 21:36:35 -0400

    Added welcome message

---
+++ app/welcome/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+    return <>
+    <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content">
+    <div className="max-w-lg text-center">
+      <h1 className="text-5xl font-bold">Hi there lover</h1>
+      <div className="py-6 text-left">
+      <p className="p-1">This is an attempt at a handmade project. It's one that I've put a fair amount of work into so I hope that you like it.</p>
+      <p className="p-1">I love you so freaking much. Like, a lot a lot.</p>
+      <p className="p-1">
+        Nothing on the next pages will make up for being so late with this, I know that.
+        I will always feel guilt about being so belated in giving you this.</p>
+      <p className="p-1">I just want you to know that I care so much about you and never ever forgot about this.</p>
+      </div>
+      <button className="btn btn-secondary text-center">Get Started</button>
+    </div>
+  </div>
+</div>
+</>
+}
